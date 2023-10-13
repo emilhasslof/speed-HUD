dc07af5279ecaa5cb80999dd8b016d9f10156d68
emilhasslof <emil@acer-laptop>
2023-10-13 14:20:54 +0200

    kinda working-ish

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import Geolocation from '@react-native-community/geolocation';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
+
 
 export default function App() {
+  const [speed, setSpeed] = useState(-1);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+
+      setInterval(async () => {
+        let loc = await Location.getCurrentPositionAsync({ accuracy: 6 });
+        let s = (loc.coords.speed * 3.6).toFixed(1)
+        setSpeed(s);
+      }, 1000)
+      /*
+      let loc = await Location.getCurrentPositionAsync();
+      if (loc) {
+        setLocation(loc)
+        setSpeed(loc.coords.speed)
+      }
+      */
+    })();
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.speedBox}>
+        <Text style={styles.speed}>{speed}</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +41,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+    backgroundColor: 'black'
   },
+  paragraph: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  speed: {
+    fontSize: 200,
+    color: 'lightblue',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+  speedBox: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '30%',
+    width: '100%',
+    transform: [{ rotateX: '0deg' }, { rotateY: '180deg' }, { rotateZ: '90deg' }],
+    backgroundColor: 'black',
+  }
 });
